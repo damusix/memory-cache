52db4f5aaf13125553a03f4a196e90bbc94e2968
Danilo Alonso <damusix@gmail.com>
2018-04-12 17:37:00 -0400

    drop old browser support, observer, etc

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = require('./lib/es6');
+module.exports = require('./lib');
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-
+const chalk = require('chalk');
 const chai = require('chai');
-const expect = chai.expect;
+const { expect, assert } = chai;
 const utils = require('./lib/utils');
+const MemoryCache = require('.');
 
 let test = {
     a: { b: 'c' },
@@ -13,19 +14,13 @@ expect(utils.pathArr).to.be.a('function');
 expect(utils.findInObj).to.be.a('function');
 expect(utils.get).to.be.a('function');
 
-let pArr = utils.pathArr('a.b');
+let pArr = utils.pathArr('a/b');
 
 expect(pArr).to.have.lengthOf(2);
 expect(utils.findInObj(test, pArr)).to.equal('c');
-expect(utils.get(test, 'a.b')).to.equal('c');
+expect(utils.get(test, 'a/b')).to.equal('c');
 
 const data = require('./data.json').slice(0,5);
-
-// Test both the es6 and es5 versions
-['.', './lib/es5', './lib/ie8'].forEach((path) => {
-
-const MemoryCache = require(path);
-
 
 expect(MemoryCache).to.be.a('function');
 
@@ -39,6 +34,33 @@ test = {
 data[0].name.last = 'Rasmussen';
 
 let cache = new MemoryCache;
+
+
+// Assert events
+
+const assertEvent = function(ev, vals) {
+
+    expect(vals).to.include.all.keys('key', 'value');
+    expect(vals).to.include.any.keys('key', 'name', 'value', 'expiresIn', 'with', 'subValue', 'nested');
+};
+
+const assertEventReset = function(vals) {
+
+    expect(vals).to.be.an('undefined');
+};
+
+const assertEventExpire = function (vals) {
+
+    expect(vals).to.have.all.keys('name', 'ms');
+}
+
+cache.on('get', assertEvent.bind({},'get'));
+cache.on('set', assertEvent.bind({},'set'));
+cache.on('merge', assertEvent.bind({},'merge'));
+cache.on('concat', assertEvent.bind({},'concat'));
+cache.on('remove', assertEvent.bind({},'remove'));
+cache.on('expire', assertEventExpire);
+cache.on('reset', assertEventReset);
 
 // Assert all functions are set
 expect(cache).to.be.an('object');
@@ -54,6 +76,11 @@ expect(cache.concat).to.be.a('function');
 expect(cache.concatIn).to.be.a('function');
 expect(cache.reset).to.be.a('function');
 expect(cache.debug).to.be.a('function');
+expect(cache.disableEvents).to.be.a('function');
+expect(cache.on).to.be.a('function');
+expect(cache.off).to.be.a('function');
+expect(cache.one).to.be.a('function');
+expect(cache.trigger).to.be.a('function');
 
 let returnValue = cache.set('test', test);
 
@@ -63,7 +90,7 @@ expect(cache.get('test')).to.be.an('object');
 expect(cache.get('test').a.b).to.equal(returnValue.a.b);
 
 // Assert return full cache
-expect(cache.get(true).test).to.equal(test);
+assert.deepEqual(cache.get(true).test, test);
 
 // Assert delete from cache
 cache.set('remove', test);
@@ -71,12 +98,13 @@ expect(cache.remove('remove')).to.equal(true);
 expect(cache.get('remove')).to.equal(undefined);
 
 // Assert find deep nested value
-expect(cache.getIn('test.a.b')).to.equal('c');
-expect(cache.getIn('test.deep.nested.obj.is.set')).to.equal('yes');
+expect(cache.getIn('test/a/b')).to.equal('c');
+expect(cache.getIn('test/deep/nested/obj/is/set')).to.equal('yes');
 
 // Assert replace deep nested value
-expect(cache.setIn('test.deep.nested.obj.is.set', true)).to.equal(true);
-expect(cache.getIn('test.deep.nested.obj.is.set')).to.equal(true);
+cache.setIn('test/deep/nested/obj/is/set', true);
+
+expect(cache.getIn('test/deep/nested/obj/is/set')).to.equal(true);
 expect(cache.get('test').deep.nested.obj.is.set).to.equal(true);
 expect(test.deep.nested.obj.is.set).to.equal(true);
 
@@ -84,15 +112,17 @@ expect(test.deep.nested.obj.is.set).to.equal(true);
 cache.set('data', data);
 expect(cache.get('data')).to.be.an('array');
 expect(cache.get('data').length).to.equal(data.length);
-expect(cache.get('data')[0]).to.equal(data[0]);
+
+assert.deepEqual(cache.get('data')[0], data[0]);
 
 // Assert find nested values
-expect(cache.getIn('data.0')).to.equal(data[0]);
-expect(cache.getIn('data.0.name.last')).to.equal('Rasmussen');
+assert.deepEqual(cache.getIn('data/0'), data[0]);
+expect(cache.getIn('data/0/name/last')).to.equal('Rasmussen');
 
 // Assert replace nested values
-expect(cache.setIn('data.0.name.last', 'Alonso')).to.equal('Alonso');
-expect(cache.getIn('data.0.name.last')).to.equal('Alonso');
+cache.setIn('data/0/name/last', 'Alonso');
+
+expect(cache.getIn('data/0/name/last')).to.equal('Alonso');
 expect(data[0].name.last).to.equal('Alonso');
 
 
@@ -102,15 +132,16 @@ let arrLength = data.length;
 cache.concat('data', [1,2,3,4,5]);
 expect(cache.get('data').length).to.not.equal(arrLength);
 
-arrLength = cache.getIn('test.arr').length;
+arrLength = cache.getIn('test/arr').length;
 
 // Assert concatenation of nested properties
-cache.concatIn('test.arr', [6,7,8,9,0]);
+cache.concatIn('test/arr', [6,7,8,9,0]);
+
 expect(cache.get('test').arr.length).to.not.equal(arrLength);
-expect(cache.getIn('test.arr').length).to.not.equal(arrLength);
-expect(cache.getIn('test.arr').length).to.equal(arrLength + 5);
-expect(cache.getIn('test.arr.8')).to.equal(9);
-expect(cache.getIn('test.arr.9')).to.equal(0);
+expect(cache.getIn('test/arr').length).to.not.equal(arrLength);
+expect(cache.getIn('test/arr').length).to.equal(arrLength + 5);
+expect(cache.getIn('test/arr/8')).to.equal(9);
+expect(cache.getIn('test/arr/9')).to.equal(0);
 
 // Assert merging of cached objects
 expect(cache.get('test')).to.not.have.property('merge');
@@ -119,9 +150,9 @@ expect(cache.get('test')).to.have.property('merge');
 expect(cache.get('test').merge).to.equal(true);
 
 // Assert merging of nested objects
-expect(cache.getIn('test.deep.nested.obj.is.deepMerged')).to.equal(undefined);
-cache.mergeIn('test.deep.nested.obj.is', { deepMerged: true });
-expect(cache.getIn('test.deep.nested.obj.is.deepMerged')).to.equal(true);
+expect(cache.getIn('test/deep/nested/obj/is/deepMerged')).to.equal(undefined);
+cache.mergeIn('test/deep/nested/obj/is', { deepMerged: true });
+expect(cache.getIn('test/deep/nested/obj/is/deepMerged')).to.equal(true);
 
 
 // Assert expiration of cached value
@@ -154,11 +185,7 @@ expect(Object.keys(cache.get(true)).length).to.equal(0);
 cache = new MemoryCache(test);
 
 expect(cache.get('a')).to.be.an('object');
-expect(cache.getIn('deep.nested.obj.is.set')).to.equal(true);
+expect(cache.getIn('deep/nested/obj/is/set')).to.equal(true);
 
-setTimeout(() => {
 
-    console.log(`No errors found with "${path}"`);
-}, 500);
-
-});
+console.log(chalk.green('OK!'));
